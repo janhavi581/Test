8ec390f5de34f6d410ccd11ab0f8dc239271fb62
Janhavi Mestry <mestryjanhavi98@gmail.com>
2026-08-27 12:11:05 +0530

    Fix TestNG listener for Cucumber

--- testselenium/src/test/java/testselenium/listeners/TestListener.java
+++ testselenium/src/test/java/testselenium/listeners/TestListener.java
@@ -3,25 +3,29 @@ package testselenium.listeners;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import org.openqa.selenium.WebDriver;
 
 import testselenium.base.LoginOrangeBaseTest;
 import testselenium.utils.ScreenshotUtil;
 
-public class TestListener implements ITestListener
-{
-	@Override
-	public void onTestFailure(ITestResult result)
-	{
-	
-		
-		System.out.println("The Failed Test: " + result.getName());
-		
-		LoginOrangeBaseTest bt =
-                (LoginOrangeBaseTest) result.getInstance();
-		
-		ScreenshotUtil.takeScreenshot(bt.driver, result.getName());
-		
-	}
-	
-}
+public class TestListener implements ITestListener {
 
+    @Override
+    public void onTestFailure(ITestResult result) {
+
+        System.out.println("The Failed Test: " + result.getName());
+
+        Object testInstance = result.getInstance();
+
+        if (testInstance instanceof LoginOrangeBaseTest) {
+
+            LoginOrangeBaseTest bt = (LoginOrangeBaseTest) testInstance;
+
+            WebDriver driver = bt.driver;
+
+            if (driver != null) {
+                ScreenshotUtil.takeScreenshot(driver, result.getName());
+            }
+        }
+    }
+}
